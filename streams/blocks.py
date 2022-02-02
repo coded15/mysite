66b3066ffb1ee924e5fb584e4ed35270e6cb2e0f
coded15 <91874598+coded15@users.noreply.github.com>
2022-02-02 15:42:16 +0530

    added "fullRichText" field and "LimitedRichText" field.

--- streams/blocks.py
+++ streams/blocks.py
@@ -1,5 +1,6 @@
 """Streamfields live in here."""
 
+from asyncio import streams
 from msilib.schema import Icon
 from re import template
 from wagtail.core import blocks
@@ -15,3 +16,29 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title & Text"
+
+
+class RichTextBlock(blocks.RichTextBlock):
+    """Rich text with all the features"""
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full RichText"
+
+
+class SimpleRichTextBlock(blocks.RichTextBlock):
+    """Rich text without (limited) all the features"""
+
+    def __init__(self, required=True, help_text=None, editor='default', features=None, validators=(), **kwargs):
+        super().__init__(**kwargs)
+        self.features = [
+            "bold",
+            "italic",
+            "link",
+        ]
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "edit"
+        label = "Simple RichText"
