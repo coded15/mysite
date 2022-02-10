41824c19170fcbabb1f1f5ecfc3481d45b5ca33e
coded15 <91874598+coded15@users.noreply.github.com>
2022-02-10 20:26:18 +0530

    added changes for cta block

--- home/models.py
+++ home/models.py
@@ -9,11 +9,23 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+from streams import blocks
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.fields import StreamField
+
 
 class HomePage(Page):
     """Home page model."""
     templates = "templates/home/home_page.html"
     max_count = 1
+
+    content = StreamField(
+        [
+            ("cta", blocks.CTABlock())
+        ],
+        null=True,
+        blank=True
+    )
 
     banner_title = models.CharField(max_length=100, blank=False, null=True)
     banner_subtitle = RichTextField(features=["bold", "italic"])
@@ -35,7 +47,9 @@ class HomePage(Page):
         FieldPanel("banner_title"),
         FieldPanel("banner_subtitle"),
         ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta")
+        PageChooserPanel("banner_cta"),
+        StreamFieldPanel("content"),
+
     ]
 
     class Meta:
