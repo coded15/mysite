3524fcbf05b5168e111bccbadbee6262801a07cf
coded15 <91874598+coded15@users.noreply.github.com>
2022-02-22 19:15:52 +0530

    added an inline panel for carousel images and multifield panels

--- home/models.py
+++ home/models.py
@@ -1,17 +1,33 @@
-import imp
 from pyexpat import features
 from tabnanny import verbose
 from turtle import home
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, InlinePanel, MultiFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.fields import StreamField
+
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the homepage carousel."""
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    panels = [
+        ImageChooserPanel("carousel_image"),
+    ]
 
 
 class HomePage(Page):
@@ -44,12 +60,17 @@ class HomePage(Page):
         related_name="+"
     )
     content_panels = Page.content_panels+[
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
+        MultiFieldPanel([
+            FieldPanel("banner_title"),
+            FieldPanel("banner_subtitle"),
+            ImageChooserPanel("banner_image"),
+            PageChooserPanel("banner_cta"),
+        ], heading="Banner Options"),
+        MultiFieldPanel([
+            InlinePanel("carousel_images", max_num=5,
+                        min_num=1, label="Image"),
+        ], heading="Carousel Images"),
         StreamFieldPanel("content"),
-
     ]
 
     class Meta:
